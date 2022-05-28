f04924430cb4cb6a2065f66cfeae35abeb844a85
marckraw <marckraw@icloud.com>
2022-05-28 17:25:54 +0200

    Add better-logging package

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import betterLogging from 'better-logging'
+betterLogging(console) // Superpowering console.logs and otherrs
 
 type Data = {
   name: string
@@ -9,5 +11,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log("This is superpowered console.log from hello.ts serverless function")
   res.status(200).json({ name: 'John Doe' })
 }
