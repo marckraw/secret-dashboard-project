34b94f6aab9faab1ad2aceec93819857790cf112
marckraw <marckraw@icloud.com>
2022-05-28 18:21:00 +0200

    Add planetscale, prisma, basic db

---
+++ prisma/seed-prod.ts
@@ -0,0 +1,30 @@
+import {prisma} from "../lib/db/prisma";
+
+async function main() {
+  try {
+    await prisma.testProjectModel.createMany({
+      data: [
+        {
+          id: 'price_1KrI4XLaAoc5YphrhlUg8SWo',
+          projectName: 'Test Foundations plan Monthly'
+        },
+        {
+          id: 'price_1KrI4iLaAoc5YphrgbJo5bcx',
+          projectName: 'Test Foundations plan Annual'
+        },
+      ],
+      skipDuplicates: true
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+main()
+    .catch(e => {
+      console.error(e)
+      process.exit(1)
+    })
+    .finally(async () => {
+      await prisma.$disconnect()
+    })
